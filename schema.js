497dc2e22250af8d4b0e9d0b7b3ee76d66161639
Noah Cardoza <noahcardoza@gmail.com>
2018-01-03 12:12:09 -0800

    Basic GraphQL schema

---
+++ schema.js
@@ -0,0 +1,70 @@
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList
+} = require('graphql');
+
+const { getXML } = require('./helpers')
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: '...',
+  fields: () => ({
+    title: {
+      type: GraphQLString,
+      resolve: (book) => {
+        const title = book.title[0]
+        return title
+      }
+    },
+    isbn: {
+      type: GraphQLString,
+      resolve: getXML('isbn')
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve: (book, args, ctx) =>
+        ctx.authorLoader.loadMany(
+          book.authors[0].author
+          .map(getXML('id'))
+        )
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: '...',
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: getXML('name')
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author, args, ctx) =>
+        ctx.bookLoader.loadMany(
+          author.books[0].book
+          .map(book => book.id[0]._)
+        )
+    }
+  })
+})
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: '...',
+    fields: () => ({
+      author: {
+        type: AuthorType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args, ctx) => ctx.authorLoader.load(args.id)
+      }
+    })
+  })
+})
